9cd230c84df91f53285b07a29e7289180f2ad00d
Falko Schumann <falko.schumann@muspellheim.de>
2019-04-25 22:31:43 +0200

    Initial commit

---
+++ Wumpus/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Wumpus
+//
+//  Created by Falko Schumann on 25.04.19.
+//  Copyright © 2019 Falko Schumann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
